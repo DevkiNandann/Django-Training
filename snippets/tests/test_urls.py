4a5434a99d6dbdcc1a42345f6b57dce364747653
DevkiNandann <devki.gupta@unthinkable.co>
2021-08-11 17:47:15 +0530

    testing

---
+++ snippets/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from snippets.views import snip_listing, snip_detail, add_new, index
+
+
+class TestUrls(SimpleTestCase):
+    def test_list_url_is_resolved(self):
+        url = reverse("snippets:list")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, snip_listing)
+
+    def test_detail_url_is_resolved(self):
+        url = reverse("snippets:detail", args=[1])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, snip_detail)
+
+    def test_addnew_url_is_resolved(self):
+        url = reverse("snippets:addnew")
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, add_new)
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,21 +1,5 @@
-"""newproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 app_name = "snippets"
@@ -26,7 +10,7 @@ urlpatterns = [
             [
                 path("snippet-list/", views.snip_listing, name="list"),
                 path("<int:id>/", views.snip_detail, name="detail"),
-                path("add-new/", views.add_new),
+                path("snippet-list/add-new", views.add_new, name="addnew"),
             ]
         ),
     )
